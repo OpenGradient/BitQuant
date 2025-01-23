bc6aa3facc941cf697ac35d2fff580b25d1e5fa0
Adam Balogh <adambalogh@adams-air.mynetworksettings.com>
2025-01-23 15:56:28 -0500

    main

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import logging
-
 from dotenv import load_dotenv
-
 from server import create_flask_app
+
+# Load environment variables
+load_dotenv()
+
+# Create the Flask app at module level
+app = create_flask_app()
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
-
-    load_dotenv()
-
-    app = create_flask_app()
     app.run(debug=True)
