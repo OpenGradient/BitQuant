b47b7279dee91360f4f2d8956cf624d1193998dc
Adam Balogh <adambalogh@adams-air.mynetworksettings.com>
2025-02-11 16:30:19 -0500

    st

--- strategies/strategy.py
+++ strategies/strategy.py
@@ -7,9 +7,15 @@ from plugins.types import Pool, Action, TokenBalance, PoolPosition
 def Strategy(ABC):
 
     @abstractmethod
+    def description() -> str:
+        """Returns strategy's description for LLM."""
+        pass
+
+    @abstractmethod
     def allocate(
         tokens: List[TokenBalance],
         available_pools: List[Pool],
         positions: List[PoolPosition],
     ) -> List[Action]:
+        """Returns suggested action based on user's holdings and available pools"""
         pass
