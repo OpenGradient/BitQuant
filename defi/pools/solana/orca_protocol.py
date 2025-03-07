84a1440b02441b255393d9d2f9683b8ccc84ad12
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2025-03-07 12:43:29 +0100

    skeleton

--- defi/pools/solana/orca_protocol.py
+++ defi/pools/solana/orca_protocol.py
@@ -1,6 +1,10 @@
+from typing import List
+
+from api.types import Pool
 from defi.pools import Protocol
 
 # https://api.orca.so/docs#/operations/listPools
 class OrcaProtocol(Protocol):
 
-    pass
+    def get_pools() -> List[Pool]:
+        pass
