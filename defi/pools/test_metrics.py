28917926d514dec29baa2924f9596173b87b58cf
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2025-03-07 12:28:25 +0100

    name

--- defi/pools/test_metrics.py
+++ defi/pools/test_metrics.py
@@ -4,7 +4,7 @@ from defi.pools.defillama_metrics import DefiLlamaMetrics
 from api.types import Chain, Pool, PoolQuery
 
 
-class TestPlugins(unittest.TestCase):
+class TestDefiLlamaMetrics(unittest.TestCase):
 
     def test_defillama(self):
         metrics = DefiLlamaMetrics()
