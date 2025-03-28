7b1257be92205946c5b24b776316341c033518d6
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2025-03-28 08:58:20 +0100

    Revert "deepseek (#42)"
    This reverts commit a74ada9d3e6fdb137b210d324ac58f5ea4884b5f.

--- agent/agent_executors.py
+++ agent/agent_executors.py
@@ -1,6 +1,6 @@
-import os
+from typing import Callable
 
-from openai import OpenAI
+
 from langgraph.prebuilt import create_react_agent
 from langgraph.graph.graph import CompiledGraph
 from langchain_openai import ChatOpenAI
@@ -8,16 +8,8 @@ from langchain_openai import ChatOpenAI
 from agent.tools import create_investor_agent_toolkit, create_analytics_agent_toolkit
 
 
-REASONING_MODEL = "deepseek/deepseek-chat-v3-0324"
-
-
 def create_suggestions_executor() -> CompiledGraph:
-    openai_model = ChatOpenAI(
-        model="gpt-3.5-turbo",
-        temperature=0.0,
-        openai_api_base="https://openrouter.ai/api/v1",
-        openai_api_key=os.getenv("OPENROUTER_API_KEY"),
-    )
+    openai_model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.0)
     agent_executor = create_react_agent(
         model=openai_model,
         tools=[],
@@ -27,12 +19,7 @@ def create_suggestions_executor() -> CompiledGraph:
 
 
 def create_investor_executor() -> CompiledGraph:
-    openai_model = ChatOpenAI(
-        model=REASONING_MODEL,
-        temperature=0.0,
-        openai_api_base="https://openrouter.ai/api/v1",
-        openai_api_key=os.getenv("OPENROUTER_API_KEY"),
-    )
+    openai_model = ChatOpenAI(model="gpt-4o-mini", temperature=0.0)
     agent_executor = create_react_agent(
         model=openai_model, tools=create_investor_agent_toolkit()
     )
@@ -41,12 +28,7 @@ def create_investor_executor() -> CompiledGraph:
 
 
 def create_analytics_executor() -> CompiledGraph:
-    openai_model = ChatOpenAI(
-        model=REASONING_MODEL,
-        temperature=0.0,
-        openai_api_base="https://openrouter.ai/api/v1",
-        openai_api_key=os.getenv("OPENROUTER_API_KEY"),
-    )
+    openai_model = ChatOpenAI(model="o3-mini")
     analytics_executor = create_react_agent(
         model=openai_model,
         tools=create_analytics_agent_toolkit(),
