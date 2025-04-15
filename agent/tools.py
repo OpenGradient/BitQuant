fe0949d07851c8a1489f81552b53199eae5937d0
Adam Balogh <adambalogh@Adams-Air.lan>
2025-04-15 16:52:31 -0400

    address

--- agent/tools.py
+++ agent/tools.py
@@ -85,14 +85,18 @@ def create_analytics_agent_toolkit(
         }
 
     return [
+        # TVL tools
         show_defi_llama_historical_global_tvl,
         show_defi_llama_historical_chain_tvl,
         show_defi_llama_top_pools,
+
+        # Price tools
         analyze_price_trend,
         max_drawdown_for_token,
         portfolio_volatility,
         analyze_wallet_portfolio,
         get_coingecko_current_price,
+
         # Token tools
         get_trending_tokens,
         evaluate_token_risk,
