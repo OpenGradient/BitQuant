70ba7789cac99bf4d9dd1f4fe7d8208400b7fb90
Michael Hsu <michael100.hsu@gmail.com>
2025-04-15 15:18:50 -0500

    remove nonce check when verifying solana signature

--- server/service.py
+++ server/service.py
@@ -2,17 +2,9 @@ from nacl.signing import VerifyKey
 from api.api_types import SolanaVerifyRequest
 from base58 import b58decode
 from server.firebase import auth
-from datetime import datetime, timedelta
-from flask import abort
 
 def verify_solana_signature(verify_request: SolanaVerifyRequest) -> str:
     try:
-        nonce = float([line.split(':')[1].strip() for line in verify_request.message.split('\n') if 'Nonce:' in line][0])
-        nonce_datetime = datetime.fromtimestamp(nonce / 1000)
-        has_minute_passed = (datetime.now() - nonce_datetime) > timedelta(minutes=1)
-        if has_minute_passed:
-            abort(401)
-
         public_key = b58decode(verify_request.address)
         signature = b58decode(verify_request.signature)
         message = verify_request.message.encode('utf-8')
