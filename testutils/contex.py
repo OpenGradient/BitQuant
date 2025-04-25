0665922fb684d0ec24324f6646ce93a00b97625d
Adam Balogh <adam@vannalabs.ai>
2025-04-25 17:20:59 -0400

    rm unused

--- testutils/contex.py
+++
@@ -1,9 +0,0 @@
-TEST_CONTEXT = {
-    "conversationHistory": [],
-    "tokens": [
-        {"amount": 100, "tokenSymbol": "SUI"},
-        {"amount": 45333, "tokenSymbol": "USDC"},
-        {"amount": 900, "tokenSymbol": "suiUSDT"},
-        {"amount": 5, "tokenSymbol": "wUSDT"},
-    ],
-}
